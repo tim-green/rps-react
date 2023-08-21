b535f04041ff8147c5e0f33eda238709e3628e38
Tim <github@timgreen.ws>
2023-08-21 12:57:54 +1000

    added button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,12 @@
+// import scss
+import "./Button.scss";
+
+function Button({ text, fill, onClick, style }) {
+	return (
+		<div className={`button ${fill ? "fill" : ""}`} onClick={onClick} style={style}>
+			{text}
+		</div>
+	);
+}
+
+export default Button;
