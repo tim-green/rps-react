389f4d32a93d8ad7640a5fb253525d86d839fc96
Tim <github@timgreen.ws>
2023-08-21 12:58:02 +1000

    added footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import { useContext } from "react";
+import Button from "../Button/Button";
+import GameOver from "../GameOver/GameOver";
+
+// import scss
+import "./Footer.scss";
+
+import GameContext from "../../context/GameContext";
+
+function Footer() {
+	const { setIsShowing, isGameOver } = useContext(GameContext);
+	return (
+		<div className="footer">
+			{isGameOver && <GameOver />}
+			<Button text="rules" onClick={() => setIsShowing(true)} />
+		</div>
+	);
+}
+
+export default Footer;
