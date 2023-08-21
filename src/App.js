5d5ce58f2e10367c29ba9831974597233457ae12
Tim <github@timgreen.ws>
2023-08-21 13:02:08 +1000

    added function for the app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 
 import { useContext } from "react";
 import "./App.css";
@@ -20,23 +18,20 @@ import GameContext from "./context/GameContext";
 
 // Function App
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const{isShowing, isPlaying} = useContext (GameContext);
+  return(
+    <>
+      {isShowing && (
+        <Overlay>
+          <Rules/>
+        </Overlay>
+      )}
+      <div className="App">
+        <Header/>
+        {!isPlaying ?<Options/>:<Picks/>}
+        <Footer/>
+      </div>
+    </>
   );
 }
 
