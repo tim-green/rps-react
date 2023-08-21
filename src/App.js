affd9d9b8307a24f12e0d0be933c05f06207486b
Tim <github@timgreen.ws>
2023-08-21 13:01:28 +1000

    imported components for app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import './App.css';
 
 import { useContext } from "react";
 import "./App.css";
+
+// import components
+import Header from "./components/Header/Header";
+import Rules from "./components/Rules/Rules";
+import Options from "./components/Options/Options";
+import Footer from "./components/Footer/Footer";
+import Picks from "./components/Picks/Picks";
+import Overlay from "./components/Overlay/Overlay";
 function App() {
   return (
     <div className="App">
