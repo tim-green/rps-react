84b50cf2ee2d918d57a8e7f0da301194bd2e7571
Tim <github@timgreen.ws>
2023-08-21 12:58:08 +1000

    added gameover component

---
+++ src/components/GameOver/GameOver.jsx
@@ -0,0 +1,28 @@
+import { useContext } from "react";
+import GameContext from "../../context/GameContext";
+import Button from "../Button/Button";
+import "./GameOver.scss";
+
+function GameOver() {
+	const { message, setMessage, setUserPick, setHousePick, setIsGameOver, setIsPlaying } = useContext(GameContext);
+
+	return (
+		<div className="game-over">
+			<span>{message}</span>
+			<Button
+				text="Play Again"
+				fill
+				style={{ padding: "15px 60px" }}
+				onClick={() => {
+					setIsPlaying(false);
+					setIsGameOver(false);
+					setMessage("");
+					setUserPick("");
+					setHousePick("");
+				}}
+			/>
+		</div>
+	);
+}
+
+export default GameOver;
